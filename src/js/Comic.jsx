b1f77b831c4e18c678f92f6c3db4a172ea32cdb2
Azhar Khan <azhar@gallop.io>
2015-01-27 20:09:03 -0500

    updated look of the grid of comics

--- src/js/Comic.jsx
+++ src/js/Comic.jsx
@@ -6,9 +6,9 @@ var Comic = React.createClass({
     render: function () {
         return (
             <div className="comic">
-              <h3>{this.props.comic.series} {this.props.comic.issue}</h3>
-              <img src={this.props.comic.cover} alt={this.props.comic.series + ' ' + this.props.comic.issue + ' cover'} />
-              <div className="info">
+              <div className="preview">
+                <h3>{this.props.comic.series} {this.props.comic.issue}</h3>
+                <img className="comic-cover" src={this.props.comic.cover} alt={this.props.comic.series + ' ' + this.props.comic.issue + ' cover'} />
                 <p className="description">{this.props.comic.description}</p>
               </div>
             </div>
