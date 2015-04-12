4342ae0bf204adbe46ee45fbe76ebcccd37099c0
Azhar Khan <azmanian.devil@gmail.com>
2015-04-12 03:09:16 -0400

    added nodemon to gulpfile, new packages

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,22 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
 var filter = require('gulp-filter');
+var nodemon = require('gulp-nodemon');
+var runSequence = require('run-sequence');
+
 
 // paths
 
 var paths = {
-  sass: ['./src/sass/**/*.sass'],
-  js: ['./src/js/**/*.js'],
-  jsx: ['./src/js/**/*.jsx'],
-  html: ['./src/**/*.html', './index.html'],
-  main: ['./src/js/main.jsx'],
-  bundle: './app/main.js'
+  sass: ['./styles/**/*.sass'],
+  js: ['./app/components/**/*.js', './app/components/**/*.jsx'],
+  main: './app/main.js'
 };
 
 // clean compiled files
 
 gulp.task('clean', function(done) {
-  del(['dist'], done);
+  del(['public'], done);
 });
 
 // server
@@ -37,7 +37,7 @@ gulp.task('server', function() {
 // sass task, runs when any sass files change
 gulp.task('styles', function() {
   return gulp.src(paths.sass)
-          .pipe(sass({sourcemap: true, sourcemapPath: './src/sass', loadPath: require('node-bourbon').includePaths }))
+          .pipe(sass({sourcemap: true, sourcemapPath: './styles', loadPath: require('node-bourbon').includePaths }))
           .pipe(gulp.dest('./public/'))
           .pipe(filter('**/*.css'))
           .pipe(reload({ stream: true }));
@@ -49,10 +49,11 @@ gulp.task('scripts', ['clean'], function() {
   return browserify({
             entries: paths.main,
             debug: true,
+            paths: ['./node_modules','./app/components/'],
             transform: [reactify]
           })
           .bundle()
-          .pipe(source(paths.bundle))
+          .pipe(source('main.js'))
           .pipe(gulp.dest('./public/'));
 });
 
@@ -61,10 +62,43 @@ gulp.task('scripts', ['clean'], function() {
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['styles']);
   gulp.watch(paths.jsx, ['scripts', reload]);
-  gulp.watch(paths.html, reload);
+  // gulp.watch(paths.html, reload);
 });
 
 // default task to be run with `gulp`
-gulp.task('default', ['clean', 'styles', 'scripts', 'server', 'watch'], function() {
+// gulp.task('default', ['clean', 'styles', 'scripts', 'server', 'watch'], function() {
+// });
+
+// build task for preparing the server
+
+gulp.task('build', ['clean', 'styles', 'scripts'], function() {
+  console.log('Build completed.');
+});
+
+// nodemon
+
+gulp.task('daemon', function() {
+  nodemon({
+    script: 'server.js',
+    ext: 'js',
+    env: {
+      'NODE_ENV': 'development'
+    }
+  })
+    .on('start', ['watch'])
+    .on('change', ['watch'])
+    .on('restart', function () {
+      console.log('restarted server...');
+    });
+});
+
+gulp.task('default', function() {
+  runSequence(
+    'clean',
+    'styles',
+    'scripts',
+    'daemon'
+  );
+
 });
 
