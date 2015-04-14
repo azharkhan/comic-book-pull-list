f25c955d195c9725d4ad2abfafdc4162b4be90e3
Azhar Khan <azmanian.devil@gmail.com>
2015-04-13 22:04:39 -0400

    calling main API, for current date

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,13 +1,23 @@
 var React = require('react');
 var ComicList = React.createFactory(require('../components/ComicList.jsx'));
 var sampleData = require('../data/sampleData.js');
+var moment = require('moment');
+var request = require('request');
 
 module.exports = function(app) {
 
 	app.get('/', function(req, res){
-		var props = { data: sampleData };
-		var comicListHtml = React.renderToString(ComicList(props));
-		res.render('home', { comicList: comicListHtml, props: JSON.stringify(props) });
+		// find the upcoming wednesday
+		var wednesday = moment().day("Wednesday");
+		var url = 'http://freshcomics.us/comics/' + wednesday.format('YYYY-MM-DD') + '/json';
+		
+		request(url, function(error, response, body) {
+			if(!error && response.statusCode == 200) {
+				var props = { data: JSON.parse(body) };
+				var comicListHtml = React.renderToString(ComicList(props));
+				res.render('home', { comicList: comicListHtml, props: JSON.stringify(props) });
+			}
+		});
 	});
 
 };
