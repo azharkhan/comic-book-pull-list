741c537eb4be2fa49293f1baab83d7c17166c9ae
Azhar Khan <azmanian.devil@gmail.com>
2015-05-12 22:31:58 -0400

    added Parse to app

--- app/main.js
+++ app/main.js
@@ -2,8 +2,13 @@
 
 var React = require('react');
 var ComicList = require('ComicList.jsx');
+var Parse = require('parse').Parse;
+var ParseReact = require('parse-react');
 
 var mountNode = document.getElementById('comic-list-mount');
 var props = JSON.parse(document.getElementById('props').innerHTML);
+
+// initialize Parse
+Parse.initialize("pXvDRqKEb4VDzfgMb32EIG8bRHpmUEibRuCa8tw1", "xdzkBYSGeQlglf3PUN4Us8DFdyPtwJwfwL9UfuYN");
 
 React.render(<ComicList data={props.data} />, mountNode);
